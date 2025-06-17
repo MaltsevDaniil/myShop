247ce674dd61290b322ecfa38a348fac39d7767b
MaltsevDaniil <ge0fftwitc@gmail.com>
2025-06-17 12:39:25 +0300

    add pdf for every order

--- orders/admin.py
+++ orders/admin.py
@@ -41,12 +41,17 @@ class OrderItemInline(admin.TabularInline):
     model = OrderItem
     raw_id_fields = ['product']
 
+def order_pdf(obj):
+    url = reverse('orders:admin_order_pdf', args=[obj.id])
+    return mark_safe(f'<a href="{url}">PDF</a>')
+order_pdf.short_description = 'Invoice'
+
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display = ['id', 'first_name', 'last_name', 'email',
                     'address', 'postal_code', 'city', 'paid',order_stripe_payment,
-                    'created', 'updated', order_detail]
+                    'created', 'updated', order_detail, order_pdf]
     list_filter = ['paid', 'created', 'updated']
     inlines = [OrderItemInline]
     actions = [export_to_csv]
--- orders/views.py
+++ orders/views.py
@@ -3,9 +3,21 @@ from .models import OrderItem, Order
 from .forms import OrderCreateForm
 from django.urls import reverse
 from django.contrib.admin.views.decorators import staff_member_required
+from django.conf import settings
+from django.http import HttpResponse
+from django.template.loader import render_to_string
 from .tasks import order_created
 from cart.cart import Cart
+import weasyprint
 
+@staff_member_required
+def admin_order_pdf(request, order_id):
+    order = get_object_or_404(Order, id=order_id)
+    html = render_to_string("orders/order/pdf.html", {'order': order})
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = f'filename=order_{order_id}.pdf'
+    weasyprint.HTML(string=html).write_pdf(response, stylesheets=[weasyprint.CSS(settings.STATIC_ROOT / 'css/pdf.css')])
+    return response
 
 def order_create(request):
     cart = Cart(request)
